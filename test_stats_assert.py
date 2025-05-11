c76f703cd98ca9cacb7b13e2942168c9add059c0
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-11 13:03:38 +0900

    assertを使った関数テストを実装

---
+++ test_stats_assert.py
@@ -0,0 +1,6 @@
+from stats_utils import find_max, find_min, get_average, get_range
+
+assert find_max([1, 2, 3]) == 3
+assert find_min([1, 2, 3]) == 1
+assert get_average([1, 2, 3]) == 2.0
+assert get_range([1, 2, 3]) == 2
