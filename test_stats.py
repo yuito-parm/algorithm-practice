02fc9bdf4f8c64f4ea92324e67bd0dcb1dffcd44
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-11 12:40:29 +0900

    統計関数に対する簡単な動作テストを実装

---
+++ test_stats.py
@@ -0,0 +1,6 @@
+from stats_utils import find_max, find_min, get_average, get_range
+
+print(f"find_maxテスト:{find_max([1, 2, 3]) == 3}")
+print(f"find_minテスト:{find_min([1, 2, 3]) == 1}")
+print(f"get_averageテスト:{get_average([1, 2, 3]) == 2.0}")
+print(f"get_rangeテスト:{get_range([1, 2, 3]) == 2}")
